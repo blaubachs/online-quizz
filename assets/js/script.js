bc5affa8a6eb876cec4943d5ce8880594c32eda6
Ben L <veloswim@gmail.com>
2022-12-23 13:07:48 -0800

    Basic layout of wireframe and some responsive CSS done

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// global vars we might need
+let questions = []
+let answers = [[1,2,3,4],["cheese","potatoes"]]
+let correct = ["literal answer or the index position of correct answers"]
+//html
+    //we can make extra html documents to use
+
+//style
+    //need to display the page
+        //need to show the question
+        //need to show the 4 boxes for answers
+        //need the questions and answers to fall in their respective box
+    //try to make it look clean
+
+//javascript
+    //function
+    //init first question
+    //start timer
+
+    //function
+    //called when answer selected
+    //checks if the answer is right or wrong
+    //deducts from timer if wrong
+    //prompts that you gave an incorrect answer, indicate to the user the answer was wrong
+    //then load next question (separate function)
+
+    //function
+    //loads next question
+
+    //function
+    //triggers end of game
+    //last question being answered or timer hits 0
+    //prompts initials
+    //saves high scores
+    //shows high scores
+    //play again button
